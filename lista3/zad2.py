882e9d4d1a3e5ac933ecc2edfc44e38ac8da07a4
Emil Olszewski <268789@student.pwr.edu.pl>
2023-11-07 12:39:55 +0100

    Poprawna metoda gaussa-seidela

--- lista3/zad2.py
+++ lista3/zad2.py
@@ -1,6 +1,11 @@
 # Zadanie 2 Lista 3. Metoda Seidla-Gaussa
+import sys
+
+sys.path.insert(1, "E:\Studia\sem5\metody_numeryczne\listy")
 import numpy as np
-from numpy import linalg as la
+import numpy.linalg as la
+from lista2.zad3 import solve_left_triangular
+
 
 # Funkcja generująca macierz Toeplitza z polecenia wymiaru n
 def toeplitz(n):
@@ -15,47 +20,40 @@ def toeplitz(n):
     A[0, n-1] = 1
     return A
 
-# Implementacja metody Gaussa-Seidla 
-def gauss_seidel(A: np.ndarray, b: np.ndarray, eps: float, max_iter=1000, initial_guess: np.ndarray = None):
+def dlu(A: np.ndarray): 
+    D = np.diag(np.diag(A))
+    L = np.tril(A, -1)
+    U = np.triu(A, 1)
 
-    # Sprawdzamy czy macierz jest diagonalnie dominująca
-    for i in range(len(A)):
-        if abs(A[i, i]) < sum(abs(A[i, :])) - abs(A[i, i]):
-            print("UWAGA!: Macierz nie jest diagonalnie dominująca")
-            break
-            
-    # Jeśli nie podamy punktu startowego to przyjmujemy wektor zerowy
-    if initial_guess is None:
-        x = np.zeros(len(A))
-    else:
-        x = initial_guess
+    return (D, L, U)
 
-    # Jeśli punkt startowy jest rozwiązaniem to kończymy
-    if (np.dot(A, x) == b).all():
-        return (x, 0)
-
+def gauss_seidel(A: np.ndarray, b: np.ndarray, initial_guess: np.ndarray, eps: float, max_iter: int=1000):
+    D, L, U = dlu(A)
+    x = initial_guess
     count = 0
+
+    if la.norm(np.identity(len(A)) - np.dot(la.inv(D+L), A)) >= 1:
+        raise ValueError("Brak zbieżności")
+
     while count < max_iter:
-        x_prev = x.copy()
-        for i in range(len(A)):
-            x[i] = (b[i] - np.dot(A[i, :i], x[:i]) - np.dot(A[i, i + 1 :], x_prev[i + 1 :])) / A[i, i]
+        x = solve_left_triangular(D + L, np.dot(-U, x) + b)
         count += 1
-        # Jeśli rozwiązanie jest dostatecznie dokładne to kończymy
         if la.norm(np.dot(A, x) - b) < eps:
             break
-    else:
-        print("Rozwiązanie nie jest zbieżne.")
+        
     return (x, count)
 
 
 if __name__ == "__main__":
 
-    a = toeplitz(20)
-    b = np.array([0 for i in range(19)] + [100])
+    dim = 10
 
-    sol, count = gauss_seidel(a, b, 0.0001)
+    a = toeplitz(dim)
+    b = np.array([0 for _ in range(dim - 1)] + [100])
+
+    sol, count = gauss_seidel(a, b, np.ones(dim), 1e-3)
     print(f"Ilość iteracji: {count}")
-    # Print the solution in non scientific notation 
+    # Pie wyświetlaj rozwiązania w notacji wykładniczej
     np.set_printoptions(suppress=True)
     print(f"Rozwiązanie: {sol.round(3)}")
     print(f"Ax = {np.dot(a, sol).round(3)}")
