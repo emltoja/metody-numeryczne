9534c904f26ae5915db78d44865a4c28657b98e1
Emil Olszewski <eemil.olszewski@gmail.com>
2024-01-20 11:21:23 +0100

    lista1. Standard IEEE

---
+++ lista1/zad1.f
@@ -0,0 +1,8 @@
+        program pade
+
+            implicit none
+            real    :: x = 21.37;
+            integer :: int = 12;
+            write(*, *) 'x = ', x, 'int = ', int;
+                
+        end program pade
